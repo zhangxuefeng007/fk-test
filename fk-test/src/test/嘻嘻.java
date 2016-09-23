4ac8bec4f02aafbf0fc43c083c20cbdafc297a1c
zxf <zhangxuefeng@xstudy.com.cn>
2016-09-23 18:00:51 +0800

    呵呵

---
+++ fk-test/src/test/嘻嘻.java
@@ -0,0 +1,28 @@
+package test;
+
+public class 嘻嘻 {
+	public static void main(String[] args) {
+		Thread a1 = new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				mm();
+			}
+			
+			synchronized void mm() {
+				try {
+					wait();
+				} catch (InterruptedException e) {
+					System.out.println("我被中断了");
+					e.printStackTrace();
+				}
+			}
+		});
+		
+		a1.start();
+		
+		//sdfsdfsd
+		
+		a1.interrupt();
+	}
+}
